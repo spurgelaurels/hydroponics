6ebb29a9674521db6ec20bda22c32524482b3350
Daphne Reed <daphne.reed@vidyard.com>
2023-02-05 13:18:45 -0500

    Added Temp/Humidity sensor

--- hydroponics.py
+++ hydroponics.py
@@ -2,12 +2,13 @@ import machine
 from machine import I2C, ADC, Pin, SoftI2C
 from lcd_api import LcdApi
 from pico_i2c_lcd import I2cLcd
-import time
+import utime as time
 from time import sleep
 import rp2
+from dht import DHT11
+
 
 class WaterLevel:
-    
     def __init__(self, level_min, level_max, ADC_Pin):
         self.level_min = level_min
         self.level_max = level_max
@@ -30,51 +31,31 @@ class WaterLevel:
             return self.percentage
 
 
+
+## Object setups
+# Setup DHT Sensor
+dhtPIN = 16
+dhtSensor = DHT11(Pin(dhtPIN, Pin.OUT, Pin.PULL_DOWN))   
+
+# Setup LCD 
 i2c = I2C(0, sda=Pin(0),scl=Pin(1), freq=100000)
 print(i2c.scan())
 I2C_ADDR = i2c.scan()[0]
 lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
 
+
+## Main Loop (TEST)
 while True:
     Tank1 = WaterLevel(39000,65535,26)
+    sleep(1)
+    dhtSensor.measure()
     lcd.clear()
-    lcd.putstr("W Lvl 1:"+ str(Tank1.percentage())+"%\n")
+    lcd.putstr("Water Lvl 1:"+ str(Tank1.percentage())+"%\n")
+    sleep(2)
+    lcd.clear()
+    lcd.putstr("Temp:"+ str(dhtSensor.temperature())+" C\n" \
+           "Hum : " + str(dhtSensor.humidity())+"%\n")
     sleep(1)
 
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  # adc_waterlevel1 = ADC(Pin(26))
-# waterlevel1_min = 30000
-# waterlevel1_max = 36488
-# 
-# adc_waterlevel2 = ADC(Pin(27))
-# adc_waterlevel3 = ADC(Pin(28))
-  
-  
-  
-#     print(I2C_ADDR)
-#     lcd.blink_cursor_on()
-#     lcd.blink_cursor_off()
-#     lcd.clear()
-#     lcd.putstr("Backlight Test")
-#     for i in range(10):
-#         lcd.backlight_on()
-#         sleep(0.2)
-#         lcd.backlight_off()
-#         sleep(0.2)
-#     lcd.backlight_on()
-#     lcd.hide_cursor()
-#     for i in range(20):
-#         lcd.putstr(str(i))
-#         sleep(0.4)
-#         lcd.clear()
         
         
