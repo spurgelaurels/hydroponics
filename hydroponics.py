756a42ad5ffa9ff8e2ad35390edde3e0240f65b7
Daphne Reed <daphne.reed@vidyard.com>
2023-02-04 22:19:50 -0500

    CLASSIFIED

--- hydroponics.py
+++ hydroponics.py
@@ -6,61 +6,62 @@ import time
 from time import sleep
 import rp2
 
-adc_waterlevel1 = ADC(Pin(26))
-waterlevel1_min = 30000
-waterlevel1_max = 36488
-
-adc_waterlevel2 = ADC(Pin(27))
-adc_waterlevel3 = ADC(Pin(28))
+class WaterLevel:
+    
+    def __init__(self, level_min, level_max, ADC_Pin):
+        self.level_min = level_min
+        self.level_max = level_max
+        self.ADC_Pin = ADC_Pin
+        
+    def percentage(self):            
+        self.level_cur = ADC(Pin(self.ADC_Pin))
+        self.level_range = self.level_max - self.level_min
+        
+        self.corrected_min = self.level_cur.read_u16() - self.level_min
+        self.percentage = (self.corrected_min * 100) / self.level_range
+        if self.percentage <= 0:
+            self.percentage = 0
+            return self.percentage
+        elif self.percentage >=100:
+            self.percentage = 100
+            return self.percentage
+        else:
+            int(self.percentage)
+            return self.percentage
 
 
 i2c = I2C(0, sda=Pin(0),scl=Pin(1), freq=100000)
 print(i2c.scan())
 I2C_ADDR = i2c.scan()[0]
-
 lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
 
 while True:
-    
-#     lcd.clear()
-#     lcd.putstr("level: "+ str(adc_waterlevel1.read_u16())+"\n")
-#     sleep(2)
-
-
-
+    Tank1 = WaterLevel(39000,65535,26)
     lcd.clear()
-    range = waterlevel1_max - waterlevel1_min
-#     lcd.putstr("range:\n"+ str(range))
-#     sleep(2)
-    
-    lcd.clear()
-    correctedStartValue = adc_waterlevel1.read_u16() - waterlevel1_min
-    lcd.putstr("level: "+ str(adc_waterlevel1.read_u16())+"\n")
-#     lcd.putstr("start: "+ str(correctedStartValue)+"\n")
-    sleep(2)
-    
-    lcd.clear()
-    percentage = (correctedStartValue * 100) / range
-    if percentage <= 0:
-        percentage = 0
-    else:
-        int(percentage)
-    
-    lcd.putstr("W Lvl 1:"+ str(percentage)+"%\n")
+    lcd.putstr("W Lvl 1:"+ str(Tank1.percentage())+"%\n")
     sleep(1)
+
+
   
   
   
+  
+  
+  
+  
+  
+  
+  # adc_waterlevel1 = ADC(Pin(26))
+# waterlevel1_min = 30000
+# waterlevel1_max = 36488
+# 
+# adc_waterlevel2 = ADC(Pin(27))
+# adc_waterlevel3 = ADC(Pin(28))
   
   
   
 #     print(I2C_ADDR)
 #     lcd.blink_cursor_on()
-#     lcd.putstr("I2C Address:"+str(I2C_ADDR)+"\n")
-#     lcd.putstr("Tom's Hardware")
-#     sleep(2)
-#     lcd.putstr("Tom's Hardware")
-#     sleep(2)
 #     lcd.blink_cursor_off()
 #     lcd.clear()
 #     lcd.putstr("Backlight Test")
